810d9b448e6749fbb0dc14092a53f5231215f549
FernandoGalende <fernandogalende@gmail.com>
2019-05-31 17:42:43 +0200

    Added build to repositori

---
+++ build/precache-manifest.b7704a454f02920881eba652c01057fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c82cdf8a1569a444b7fa216265f3c5",
+    "url": "/hello-world-ci-cd/index.html"
+  },
+  {
+    "revision": "29c142de86b38dbbdbc2",
+    "url": "/hello-world-ci-cd/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/hello-world-ci-cd/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "29c142de86b38dbbdbc2",
+    "url": "/hello-world-ci-cd/static/js/main.b57f72f1.chunk.js"
+  },
+  {
+    "revision": "142d53815eab8cc2e1c2",
+    "url": "/hello-world-ci-cd/static/js/runtime~main.66adea25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world-ci-cd/static/media/logo.5d5d9eef.svg"
+  }
+]);
